fe08f7390051276861c1f4a99bb5d44ce4376c06
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-04-15 13:54:35 +0200

    init

---
+++ js/ultima.js
@@ -0,0 +1,182 @@
+calcoloUltima = [
+    {
+        value: "0",
+        pari: 0,
+        dispari: 1
+    },
+    {
+        value: "1",
+        pari: 1,
+        dispari: 0
+    },
+    {
+        value: "2",
+        pari: 2,
+        dispari: 5
+    },
+    {
+        value: "3",
+        pari: 3,
+        dispari: 7
+    },
+    {
+        value: "4",
+        pari: 4,
+        dispari: 9
+    },
+    {
+        value: "5",
+        pari: 5,
+        dispari: 13
+    },
+    {
+        value: "6",
+        pari: 6,
+        dispari: 15
+    },
+    {
+        value: "7",
+        pari: 7,
+        dispari: 17
+    },
+    {
+        value: "8",
+        pari: 8,
+        dispari: 19
+    },
+    {
+        value: "9",
+        pari: 9,
+        dispari: 21
+    },
+    {
+        value: "a",
+        pari: 0,
+        dispari: 1
+    },
+    {
+        value: "b",
+        pari: 1,
+        dispari: 0
+    },
+    {
+        value: "c",
+        pari: 2,
+        dispari: 5
+    },
+    {
+        value: "d",
+        pari: 3,
+        dispari: 7
+    },
+    {
+        value: "e",
+        pari: 4,
+        dispari: 9
+    },
+    {
+        value: "f",
+        pari: 5,
+        dispari: 13
+    },
+    {
+        value: "g",
+        pari: 6,
+        dispari: 15
+    },
+    {
+        value: "h",
+        pari: 7,
+        dispari: 17
+    },
+    {
+        value: "i",
+        pari: 8,
+        dispari: 19
+    },
+    {
+        value: "j",
+        pari: 9,
+        dispari: 21
+    },
+    {
+        value: "k",
+        pari: 10,
+        dispari: 2
+    },
+    {
+        value: "l",
+        pari: 11,
+        dispari: 4
+    },
+    {
+        value: "m",
+        pari: 12,
+        dispari: 18
+    },
+    {
+        value: "n",
+        pari: 13,
+        dispari: 20
+    },
+    {
+        value: "o",
+        pari: 14,
+        dispari: 11
+    },
+    {
+        value: "p",
+        pari: 15,
+        dispari: 3
+    },
+    {
+        value: "q",
+        pari: 16,
+        dispari: 6
+    },
+    {
+        value: "r",
+        pari: 17,
+        dispari: 8
+    },
+    {
+        value: "s",
+        pari: 18,
+        dispari: 12
+    },
+    {
+        value: "t",
+        pari: 19,
+        dispari: 14
+    },
+    {
+        value: "u",
+        pari: 20,
+        dispari: 16
+    },
+    {
+        value: "v",
+        pari: 21,
+        dispari: 10
+    },
+    {
+        value: "w",
+        pari: 22,
+        dispari: 22
+    },
+    {
+        value: "x",
+        pari: 23,
+        dispari: 25
+    },
+    {
+        value: "y",
+        pari: 24,
+        dispari: 24
+    },
+    {
+        value: "z",
+        pari: 25,
+        dispari: 23
+    },
+]
